874755007bf731c94cf67abf93f9a847e923a6cf
Keshab <keshabkaushal@gmail.com>
2023-11-29 17:41:54 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import "aos/dist/aos.css";
+import AOS from "aos";
+
+// // init AOS animation
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
+
+// the additional code you saw in the video will not be needed :)
